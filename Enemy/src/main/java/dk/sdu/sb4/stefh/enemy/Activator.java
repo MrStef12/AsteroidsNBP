57ecf715a06736dedea7c86371158ad84b9c8df0
Steffen Hansen <stefh14@student.sdu.dk>
2016-05-12 09:47:20 +0200

    Added @Override tags

--- Enemy/src/main/java/dk/sdu/sb4/stefh/enemy/Activator.java
+++ Enemy/src/main/java/dk/sdu/sb4/stefh/enemy/Activator.java
@@ -7,11 +7,13 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+    @Override
     public void start(BundleContext context) throws Exception {
         context.registerService(IGamePluginService.class, new EntityPlugin(), null);
         context.registerService(IEntityProcessingService.class, new EnemyControlSystem(), null);
     }
 
+    @Override
     public void stop(BundleContext context) throws Exception {
         // TODO add deactivation code here
     }
